f2df32ab16239cdfe91d08c3adca4fa50bc38a5f
patrick <github@ph-f.nl>
2017-05-22 12:06:08 +0200

    Basic test ejabberd 17.01

--- src/mod_offline_http_post_group.erl
+++ src/mod_offline_http_post_group.erl
@@ -11,7 +11,7 @@
 -behaviour(gen_mod).
 
 -export([start/2, stop/1, depends/2]).
--export([mod_opt_type/1, muc_filter_message/3]).
+-export([mod_opt_type/1, muc_filter_message/5]).
 
 -include("xmpp.hrl").
 -include("logger.hrl").
@@ -38,26 +38,6 @@ depends(_Host, _Opts) ->
 mod_opt_type(_) ->
   [].
 
--spec muc_filter_message(message(), mod_muc_room:state(), binary()) -> message().
-
-muc_filter_message(Pkt, #state{config = Config, jid = RoomJID} = MUCState, FromNick) ->
-  From = xmpp:get_from(Pkt),
-
-  %% loop through room members
-  %% if is-offline
-  %% post_offline_message(From, _To, Body, Packet#message.id);
-  %% end..
-
-  ?INFO_MSG("muc_filter_message called.", []).
-
-
-%%post_offline_message(From, To, Body, MessageId) ->
-%%  Token = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, auth_token, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
-%%  PostUrl = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, post_url, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
-%%  ToUser = To#jid.luser,
-%%  FromUser = From#jid.luser,
-%%  Vhost = To#jid.lserver,
-%%  Data = string:join(["to=", binary_to_list(ToUser), "&from=", binary_to_list(FromUser), "&vhost=", binary_to_list(Vhost), "&body=", binary_to_list(Body), "&messageId=", binary_to_list(MessageId)], ""),
-%%  Request = {binary_to_list(PostUrl), [{"Authorization", binary_to_list(Token)}], "application/x-www-form-urlencoded", Data},
-%%  httpc:request(post, Request,[],[]),
-%%  ?INFO_MSG("post request sent", []).
+-spec muc_filter_message(message(), mod_muc_room:state(), jid(), jid(), binary()) -> message().
+muc_filter_message(Pkt, #state{config = Config} = MUCState, RoomJID, From, FromNick) ->
+  [].
